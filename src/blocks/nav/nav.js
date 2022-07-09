b49376f96bfd4cf6d34c82c848b8f7f5da6fb13c
Shakhrom Mukumov <leon9208@mail.ru>
2022-07-09 20:06:29 +0300

    desctop nav modified

--- src/blocks/nav/nav.js
+++ src/blocks/nav/nav.js
@@ -6,4 +6,24 @@ document.querySelector('.mobile-btn').addEventListener('click', function() {
 		this.classList.add('active')
 		document.querySelector('.mobile-nav').classList.add('active')
 	}
-})
+});
+
+// .nav-js
+
+var nav = priorityNav.init({
+	initClass: "nav-js",
+	mainNavWrapper: ".nav-wrapper",
+	breakPoint: 0,
+	navDropdownClassName: "nav-dropdown",
+	navDropdownLabel: "...",
+	throttleDelay: '50'
+	// breakPoint: 1160,
+	// initClass: "nav-js",
+	// mainNavWrapper: "nav-wrapper",
+	// mainNav: "ul",
+	// navDropdownToggleClassName: "nav__dropdown-toggle",
+	// navDropdownBreakpointLabel: "menu",
+	// throttleDelay: 50,
+	// offsetPixels: 0,
+	// count: true,
+});
