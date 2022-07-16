d9c7f8c6b3b64395f4b121c46d711ec32782b002
Shakhrom Mukumov <leon9208@mail.ru>
2022-07-16 14:15:21 +0300

    Custom quantity

--- src/blocks/form/form.js
+++ src/blocks/form/form.js
@@ -1,7 +1,6 @@
-//form
-
-var cSelect = document.querySelectorAll('.select-current')
-var cSelectItem = document.querySelectorAll('.select-item')
+//custom select
+var cSelect = document.querySelectorAll('.select-current');
+var cSelectItem = document.querySelectorAll('.select-item');
 
 if(cSelect.length > 0) {
 	cSelect.forEach(item => {
@@ -22,6 +21,31 @@ if(cSelect.length > 0) {
 			thCurrent.innerHTML = el.innerHTML
 			thCurrent.classList.remove('isactive')
 			thInput.value = el.dataset.value
+		})
+	})
+};
+
+//quantity
+var qty = document.querySelectorAll('.quantity');
+
+if (qty.length > 0) {
+	qty.forEach(qt => {
+		let qtDel = qt.querySelector('.quantity-btn--left')
+		let qtAdd = qt.querySelector('.quantity-btn--right')
+		let qtInput = qt.querySelector('.quantity-input')
+		let qtInputMin = parseFloat(qtInput.min)
+		let qtInputMax = parseFloat(qtInput.max)
+
+		qtDel.addEventListener('click', ()=> {
+			if (qtInputMin < parseFloat(qtInput.value)) {
+				qtInput.value = parseInt(qtInput.value) -1
+			}
+		})
+
+		qtAdd.addEventListener('click', ()=> {
+			if (parseFloat(qtInput.value) < qtInputMax) {
+				qtInput.value = parseInt(qtInput.value) +1
+			}
 		})
 	})
 };
